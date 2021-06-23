8aa5f945b367c068d72171bb77671396ce828e39
Driccioli <driccioli53@gmail.com>
2021-06-23 16:42:47 +0200

    Commit

---
+++ latte.js
@@ -0,0 +1,26 @@
+//Ti sei appena svegliato, è il momento di bere un bel bicchiere di latte caldo.
+/*
+1) passo da sdraiato a seduto sul letto 
+2)mi giro da un lato, se trovo le ciabatte le indosso
+3)altrimenti mi giro dal lato opposto
+4)indosso le ciabatte
+5)mi sposto in cucina
+6)apro il frigo e prendo il latte
+7)metto il latte sul tavolo e chiudo il frigo
+8)apro la dispensa e prendo il pentolino
+9)metto il pentolino sul fornetto e chiudo la dispensa
+10)apro il cartone di latte togliendo il tappo
+11)tengo fermo il pentolino con una mano
+12)e verso il latte nel pentolino con l'altra
+13)accendo il fornetto attivando prima il gas e poi premendo il tasto per la scintilla
+14)aspetto 2 minuti
+15)(opzionale: prendo il miele dalla dispensa e un cucchiaio, riempio il cucchiaio col miele e metto il cucchiaio nel pentolino)
+16)dopo 2 minuti spengo il fornetto (e tolgo il cucchiaio di miele)
+17)prendo una tazza vuota dalla dispensa
+18)verso il latte dal pentolino alla tazza
+19)alzo la tazza, apro la bocca, verso il latte dalla tazza allo stomaco
+20)metto la tazza e il pentolino nella lavastoviglie
+
+Funzioni possibili: 
+openCloseBox(box) per aprire/chiudere un contenitore
+versa(box1,box2) per versare il contenuto di box1 dentro box2 fino a quando box2 non raggiunge capacità massima
